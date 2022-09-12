29bac125991f2ca5244fbfdd99406b45968873fe
Kristian <codingwithak222@gmail.com>
2022-09-12 09:53:06 -0400

    integrated service with angular component

--- src/app/component/product-list/product-list.component.ts
+++ src/app/component/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+//inject product service into component
+  constructor( private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProducts();
   }
-
+   listProducts(){
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+   }
 }
+
+//integrates service with angular component
